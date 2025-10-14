4cb17d8f3a3ddcb85f3980942032c8d957217ca7
vihgo <vi.gomeza@profesor.duoc.cl>
2025-10-14 18:03:08 -0300

    Se agrego la clase UserManager.kt para manejar el guardado de los datos del usuario.

--- app/src/main/java/com/example/teacherstore/model/UserManager.kt
+++ app/src/main/java/com/example/teacherstore/model/UserManager.kt
@@ -8,6 +8,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 
 private  const val PREFERENCIAS_USUARIO="preferencias_usuario"
@@ -38,6 +39,18 @@ object UserManager {
     //DataStore expone los datos a través de Kotlin Flow, permitiendo observar los cambios
     val flujoUsuario: (Context) -> Flow<Map<String, Any?>> = {
         context->
+        //data es un Flow<Preferences> que emite las preferencias actuales.
+        context.dataStore.data
+            //map transforma el objeto Preferences en un Mapa(SIMILAR A DATA CLASS)
+            .map {
+                preferencias->
+                mapOf(
+                    "nombre" to preferencias[clavesPreferencias.NOMBRE_USUARIO],
+                    "email" to preferencias[clavesPreferencias.EMAIL_USUARIO],
+                    "autenticado" to preferencias[clavesPreferencias.ESTA_AUTENTICADO]
+                )
+            }
+        //data es un Flow<Preferences> que emite las preferencias actuales
         //context.dataStore.data
     }
 
