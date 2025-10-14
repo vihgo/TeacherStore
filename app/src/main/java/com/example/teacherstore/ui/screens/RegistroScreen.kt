0c078775a8f146e883a3253c0780d2530af6680a
vihgo <vi.gomeza@profesor.duoc.cl>
2025-10-14 15:47:21 -0300

    Se agrego un scaffold a la pantalla de registro.

--- app/src/main/java/com/example/teacherstore/ui/screens/RegistroScreen.kt
+++ app/src/main/java/com/example/teacherstore/ui/screens/RegistroScreen.kt
@@ -1,6 +1,7 @@
 package com.example.teacherstore.ui.screens
 
 import android.R
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -16,6 +17,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -35,105 +37,117 @@ fun RegistroScreen(
     navController: NavController
 ){
     val estado by viewModel.estado.collectAsState()
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(16 .dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold(
+        containerColor = Color.White, // Set your desired background color here
+        content = { paddingValues ->
+            // Your content goes here
+           // Text(text = "Hello, Scaffold!", modifier = androidx.compose.ui.Modifier.padding(paddingValues))
 
-    ){
-        Text(estado.nombre)
-        OutlinedTextField(
-            value = estado.nombre,
-            onValueChange = viewModel::onNombreChange,
-            label = {Text("Nombre")},
-            isError = estado.errores.nombre!=null,
-            singleLine = true,
-            supportingText = {
-                estado.errores.nombre?.let{
-                    Text(it, color = MaterialTheme.colorScheme.error)
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(16 .dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+
+            ){
+                Text(estado.nombre)
+                OutlinedTextField(
+                    value = estado.nombre,
+                    onValueChange = viewModel::onNombreChange,
+                    label = {Text("Nombre")},
+                    isError = estado.errores.nombre!=null,
+                    singleLine = true,
+                    supportingText = {
+                        estado.errores.nombre?.let{
+                            Text(it, color = MaterialTheme.colorScheme.error)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+
+
+                )
+
+                //campo para el correo
+                OutlinedTextField(
+                    value = estado.correo,
+                    onValueChange = viewModel::onCorreoChange,
+                    label = {Text("Email")},
+                    isError = estado.errores.correo!=null,
+                    singleLine = true,
+                    supportingText = {
+                        estado.errores.correo?.let{
+                            Text(it, color = MaterialTheme.colorScheme.error)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+
+
+                )
+                //campo contraseña
+
+                OutlinedTextField(
+                    value = estado.contrasena,
+                    onValueChange = viewModel::onContrasenaChange,
+                    label = {Text("Contraseña")},
+                    isError = estado.errores.contrasena!=null,
+                    visualTransformation = PasswordVisualTransformation(),
+                    supportingText = {
+                        estado.errores.contrasena?.let{
+                            Text(it, color = MaterialTheme.colorScheme.error)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+
+
+                )
+
+                OutlinedTextField(
+                    value = estado.direccion,
+                    onValueChange = viewModel::onDireccionChange,
+                    label = {Text("Dirección")},
+                    isError = estado.errores.direccion!=null,
+                    singleLine = true,
+                    supportingText = {
+                        estado.errores.direccion?.let{
+                            Text(it, color = MaterialTheme.colorScheme.error)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+
+
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = estado.aceptaTerminos,
+                        onCheckedChange = viewModel::onAceptarTerminosChange
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Text("Acepto los términos y condiciones")
+
                 }
-            },
-            modifier = Modifier.fillMaxWidth()
+                Button(
+                    onClick = {
+                        if (viewModel.estaValidadoElFormulario() && estado.aceptaTerminos) {
+                            //El nav controller debería ir a la pantalla de resumen
+                            navController.navigate(AppRoute.Home.route)
 
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
 
-        )
-
-        //campo para el correo
-        OutlinedTextField(
-            value = estado.correo,
-            onValueChange = viewModel::onCorreoChange,
-            label = {Text("Email")},
-            isError = estado.errores.correo!=null,
-            singleLine = true,
-            supportingText = {
-                estado.errores.correo?.let{
-                    Text(it, color = MaterialTheme.colorScheme.error)
+                ) {
+                    Text("Registrar")
                 }
-            },
-            modifier = Modifier.fillMaxWidth()
 
-
-        )
-        //campo contraseña
-
-        OutlinedTextField(
-            value = estado.contrasena,
-            onValueChange = viewModel::onContrasenaChange,
-            label = {Text("Contraseña")},
-            isError = estado.errores.contrasena!=null,
-            visualTransformation = PasswordVisualTransformation(),
-            supportingText = {
-                estado.errores.contrasena?.let{
-                    Text(it, color = MaterialTheme.colorScheme.error)
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
-
-
-        )
-
-        OutlinedTextField(
-            value = estado.direccion,
-            onValueChange = viewModel::onDireccionChange,
-            label = {Text("Dirección")},
-            isError = estado.errores.direccion!=null,
-            singleLine = true,
-            supportingText = {
-                estado.errores.direccion?.let{
-                    Text(it, color = MaterialTheme.colorScheme.error)
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
-
-
-        )
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(
-                checked = estado.aceptaTerminos,
-                onCheckedChange = viewModel::onAceptarTerminosChange
-            )
-            Spacer(Modifier.width(8.dp))
-            Text("Acepto los términos y condiciones")
+            }
 
         }
-        Button(
-            onClick = {
-                if (viewModel.estaValidadoElFormulario() && estado.aceptaTerminos) {
-                    //El nav controller debería ir a la pantalla de resumen
-                    navController.navigate(AppRoute.Home.route)
 
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+    )
 
-        ) {
-            Text("Registrar")
-        }
-
-    }
 
 
 }
