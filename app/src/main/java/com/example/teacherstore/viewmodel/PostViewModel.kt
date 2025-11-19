46981b7f42340ed67246b76ffa4f069cb5627a17
vihgo <vi.gomeza@profesor.duoc.cl>
2025-11-19 20:55:29 -0300

    Realizando la guía para el testeo. Falta desde el factory.

--- app/src/main/java/com/example/teacherstore/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/example/teacherstore/viewmodel/PostViewModel.kt
@@ -4,12 +4,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.teacherstore.data.model.Post
 import com.example.teacherstore.repository.PostRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class PostViewModel: ViewModel() {
-    private val repository= PostRepository()
+open class PostViewModel(
+
+    private val repository: PostRepository,
+    private val dispatcher: CoroutineDispatcher
+
+)
+    : ViewModel() {
+
     private val _postList= MutableStateFlow<List<Post>>(emptyList())
 
     val postList: StateFlow<List<Post>> =  _postList
@@ -18,8 +25,8 @@ class PostViewModel: ViewModel() {
         fetchPosts()
     }
 
-    private fun fetchPosts(){
-        viewModelScope.launch {
+    open fun fetchPosts(){
+        viewModelScope.launch(dispatcher) {
             try {
                 _postList.value=repository.getPosts()
             }catch (e: Exception){
