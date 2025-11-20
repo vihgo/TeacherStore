123f10284b6e7946f15b8422b320c2ff80fa16e0
vihgo <vi.gomeza@profesor.duoc.cl>
2025-11-20 17:25:03 -0300

    Se realizaron los cambios para la nueva lista de post pero falta cambiar la main activity

--- app/src/main/java/com/example/teacherstore/ui/screens/PostScreen.kt
+++ app/src/main/java/com/example/teacherstore/ui/screens/PostScreen.kt
@@ -1,6 +1,5 @@
 package com.example.teacherstore.ui.screens
 
-import android.R
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -21,61 +20,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import com.example.teacherstore.viewmodel.PostViewModel
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PostScreen(viewModel: PostViewModel){
-    //observamos el flujo de datos del ViewModel
+    val posts = viewModel.postList.collectAsState().value
 
-    val posts= viewModel.postList.collectAsState().value
-
-    Scaffold(topBar = {
-        TopAppBar(title = {Text("Ultimos Posts")})
-    })
-    {
-        innerPadding ->
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Listado de posts")
+                }
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
         ){
-            LazyColumn(modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-
-
-            ){
-                items(posts){
-                    post->
-                    Card(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-
-                    ) {
-                        Column(modifier = Modifier.padding(16.dp))
-                        {
-                            Text(text = "Titulo: ${post.title}",
-                                style = MaterialTheme.typography.titleMedium
-                                )
-
-                            Spacer(modifier = Modifier.height(4.dp))
-
-                            Text(text = post.body,
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-
+            LazyColumn(
+                modifier = Modifier.fillMaxSize().padding(all = 16.dp)
+            ) { items(posts){ post->
+                Card(modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp), elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+                    Column (modifier = Modifier.padding(all = 16.dp)){
+                        Text(text = "Titulo: ${post.title}", style = MaterialTheme.typography.titleMedium)
+                        Spacer(modifier = Modifier.height(height = 4.dp))
+                        Text(text = "Post: ${post.body}", style = MaterialTheme.typography.bodyMedium)
                     }
                 }
-            }
-
+            } }
         }
-
-
-
     }
-
-
 }
